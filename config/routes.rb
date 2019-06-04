e60bca9ec93a1e20bc2c18a1cae8e51853b03fdf
Rafael Martínez Abraham <martinezkind@gmail.comm>
2019-06-04 12:38:32 -0400

    destroy y correcciones

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,11 @@ post 'todos', to: 'todos#create'
 
 get 'todos/:id', to: 'todos#show', as:'todo'
 
+get 'todos/:id/edit', to: 'todos#edit', as:'todo_edit'
+
+patch 'todos/:id', to: 'todos#update'
+
+delete 'todos/:id', to: 'todos#destroy'
+
 
 end
